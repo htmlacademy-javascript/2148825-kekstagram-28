c9c2e7d1e50c28ffd3599bc12ec78a8440818563
MedeAnaCode <medentsova.anastasia@yandex.ru>
2023-02-28 22:11:20 +0300

    adds function renderPhotos

--- js/pictures.js
+++ js/pictures.js
@@ -1,16 +1,19 @@
 import {generatePhotos} from './generate-data.js';
 
-const photoData = generatePhotos(25);
+
 const picturesContainer = document.querySelector('.pictures');
 const picturesTemplate = document.querySelector('#picture').content.querySelector('.picture');
 const picturesFragment = document.createDocumentFragment();
+const renderPhotos = (data) => {
+  data.forEach(({url, likes, comments}) => {
+    const picture = picturesTemplate.cloneNode(true);
+    picture.querySelector('.picture__img').src = url;
+    picture.querySelector('.picture__comments').textContent = comments.length;
+    picture.querySelector('.picture__likes').textContent = likes;
+    picturesFragment.append(picture);
+  });
 
-photoData.forEach(({url, likes, comments}) => {
-  const picture = picturesTemplate.cloneNode(true);
-  picture.querySelector('.picture__img').src = url;
-  picture.querySelector('.picture__comments').textContent = comments.length;
-  picture.querySelector('.picture__likes').textContent = likes;
-  picturesFragment.append(picture);
-});
+  picturesContainer.append(picturesFragment);
+};
 
-picturesContainer.append(picturesFragment);
+export {renderPhotos};
