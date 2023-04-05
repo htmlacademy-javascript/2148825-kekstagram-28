1f3e5535b0c7499c71ab03be3184b5a6cc091926
MedeAnaCode <medentsova.anastasia@yandex.ru>
2023-04-05 17:21:54 +0300

    corrects comments

--- js/filter.js
+++ js/filter.js
@@ -9,15 +9,19 @@ const imgFilters = document.querySelector('.img-filters');
 const buttons = document.querySelectorAll('.img-filters__button');
 const buttonsContainer = document.querySelector('.img-filters__form');
 
-const showFilter = () => imgFilters.classList.remove('img-filters--inactive');
+const Filter = {
+  'filter-default': (photos) => photos,
+  'filter-random': (photos) => generateNewRandomPhotoData(photos),
+  'filter-discussed': (photos) => generateDiscussedPhotoData(photos),
+};
 
-const generateNewRandomPhotoData = (array) => {
+const generateNewRandomPhotoData = (sourceArray) => {
   const getRandomId = getRandomUniqueNumber(0, NUMBER_OF_PHOTOS - 1);
   const newRandomPhotoData = [];
 
   for (let i = 0; i < NUMBER_OF_RANDOM_PHOTOS; ++i) {
     const number = getRandomId();
-    newRandomPhotoData.push(array[number]);
+    newRandomPhotoData.push(sourceArray[number]);
   }
 
   return newRandomPhotoData;
@@ -25,45 +29,36 @@ const generateNewRandomPhotoData = (array) => {
 
 const generateDiscussedPhotoData = (sourceArray) => sourceArray.slice().sort((a, b) => b.comments.length - a.comments.length);
 
+
+const showFilter = () => imgFilters.classList.remove('img-filters--inactive');
+
 const clearPictures = () => {
   const pictures = document.querySelectorAll('.picture');
   pictures.forEach((element) => element.remove());
-};
-
-const renderNewPhotos = (sourceData, cb = (el) => el) => {
-  clearPictures();
-  renderPhotos(cb(sourceData));
-};
-
-const Callbacks = {
-  'filter-default': (photos) => renderNewPhotos(photos),
-  'filter-random': (photos) => renderNewPhotos(photos, generateNewRandomPhotoData),
-  'filter-discussed': (photos) => renderNewPhotos(photos, generateDiscussedPhotoData)
 };
 
 const clearButtons = () => {
   buttons.forEach((button) => button.classList.remove('img-filters__button--active'));
 };
 
-const changeButtons = (evt) => {
-  clearButtons();
-  const element = evt.target.closest('.img-filters__button');
-  element.classList.add('img-filters__button--active');
-};
+const setFilterListener = (data) => {
+  buttonsContainer.addEventListener('click', debounce((evt) => {
+    const targetButton = evt.target.closest('.img-filters__button');
 
-const setButtonsContainerListeners = (data) => {
-  buttonsContainer.addEventListener('click', (evt) => {
-    const targetButton = evt.target.closest('.img-filters__button').id;
-    const cb = Callbacks[targetButton];
-    const cbDebounced = debounce(() => cb(data));
-    cbDebounced(data);
-  });
+    if (targetButton) {
+      clearButtons();
+      targetButton.classList.add('img-filters__button--active');
+      const filterFn = Filter[targetButton.id];
+      const filterPhotos = filterFn(data);
+      clearPictures();
+      renderPhotos(filterPhotos);
+    }
+  }));
 };
 
 const initFilter = (data) => {
   showFilter();
-  buttons.forEach((button) => button.addEventListener('click', (evt) => changeButtons(evt)));
-  setButtonsContainerListeners(data);
+  setFilterListener(data);
 };
 
 export {initFilter};
